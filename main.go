fda43b7f66a291f741ed6e5cf43bf1e69e740137
Rob Gonnella <robgonnella@gmail.com>
2020-02-01 14:21:38 -0800

    Initial commit

---
+++ main.go
@@ -0,0 +1,15 @@
+package zipcodes
+
+//go:generate go run gen.go
+type Zipcodes struct {}
+
+func (z Zipcodes) New() Zipcodes {
+	return Zipcodes{}
+}
+
+func (z Zipcodes) Lookup(zip string) ZipcodeData {
+	if data, ok := zipcodes[zip]; ok {
+	    return data
+	}
+	return ZipcodeData{}
+}
